70185be257685c337ff389c0b4caa2adb4786fce
janracz <raczynskijasiek@gmail..com>
2020-01-20 19:14:40 +0100

    First Commit

---
+++ app/src/main/java/com/raczynski/firstproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.raczynski.firstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
